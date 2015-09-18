2db097c977e173a63316341e6ae487893b7be531
Jonathan Garbee <jonathan@garbee.me>
2015-09-18 10:12:44 -0400

    Initial commit.

---
+++ tests/lib/rules/no-innerText.js
@@ -0,0 +1,24 @@
+/**
+ * @fileoverview Tests for no-innerText rule.
+ * @author Jonathan Garbee
+ * @copyright 2015 Jonathan Garbee. All rights reserved.
+ **/
+
+'use strict';
+
+var rule = require('../../../lib/rules/no-innerText'),
+    RuleTester = require('../../../node_modules/eslint/lib/testers/rule-tester');
+
+var ruleTester = new RuleTester();
+ruleTester.run('no-innerText', rule, {
+    valid: [
+        'foo.textContent = "hello"'
+    ],
+    invalid: [
+        {
+            code: 'foo.innerText = "hello"',
+            errors: [{ message: 'innerText does not work in Firefox. Use textContent instead.', type: 'ExpressionStatement'}]
+        }
+    ]
+});
+
